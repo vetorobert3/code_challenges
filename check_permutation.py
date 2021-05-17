ab75b8981123030b9f3d0b494c6b28fa6fea862b
Veto Ramirez <vetorobert3@gmail.com>
2021-05-17 12:11:28 -0700

    did check_permutations

--- check_permutation.py
+++ check_permutation.py
@@ -11,3 +11,25 @@ Understand:
 Plan:
     First, I would create a dictionary to house all the characters from the first string. Than I would iterate through the second string. If the characters from the second string match, then I would add one to the total number from first iteratation of th first string. If the number of...
 """
+from collections import Counter
+
+def check_perms(s1, s2):
+    if len(s1) != len(s2):
+        return False
+
+    counter = Counter()
+
+    for i in s1:
+        counter[i] += 1
+    for i in s2:
+        if counter[i] == 0:
+            return False
+        counter[i] -= 1
+
+    return True
+
+
+string_1 = "asdf"
+string_2 = "asef"
+
+print(check_perms(string_1, string_2))
