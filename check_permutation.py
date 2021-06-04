2a44745c38df75a6e04ee8bdf7e5c9d58f575c1e
Veto Ramirez <vetorobert3@gmail.com>
2021-06-03 20:41:03 -0700

    Made revisions to the code. Instead of [i] which refers to a index, I made it [char] refering to a characrter.

--- check_permutation.py
+++ check_permutation.py
@@ -1,34 +1,42 @@
 """
-In this challenge, we will check to see if two strings are permutations of each other. That is, if the strings contain the same characters but just in different order.
+    In this challenge, we will check to see if two strings are permutations of each other. That is, if the strings contain the same characters but just in different order.
 
-Understand:
-    Input: ("abcd", "bdca")
-    Output: True
-
-    Input: ("abcd", "1234")
-    Output: False
-
-Plan:
-    First, I would create a dictionary to house all the characters from the first string. Than I would iterate through the second string. If the characters from the second string match, then I would add one to the total number from first iteratation of th first string. If the number of...
+    Understand:
+        Input: ("qwer", "rewq)
+        Output: True
+        
+        Input: ("qwer", "12ab")
+        Output: False
+        
+    Plan:
+        We will create a counter. The counter will keep track of how many time a character occurs in the first iteration of the first string. We will than iterate through the second string and subtract those characters from the ones already in the counter. If values are 0 then we have a permutation. If values are > or < 0 then we don't have a permutation.
 """
 from collections import Counter
 
 def check_perms(s1, s2):
+    # If the lengths are not the same for both strings, then there can be no perm
     if len(s1) != len(s2):
         return False
-
+        
+    # Init counter
     counter = Counter()
-
-    for i in s1:
-        counter[i] += 1
-    for i in s2:
-        if counter[i] == 0:
+    
+    # Add values to the counter by iterating through s1
+    for char in s1:
+        counter[char] += 1
+        
+    # Iterating through s2
+    for char in s2:
+        # If counter[i] is zero, then it wasn't in s1
+        if counter[char] == 0:
+            # No permutatuion
             return False
-        counter[i] -= 1
-
+        # Else, subtract the value already in the counter
+        counter[char] -= 1
+    
+    # This will make it a True statement    
     return True
-
-
+    
 string_1 = "asddf"
 string_2 = "asddf"
 print(check_perms(string_1, string_2))
